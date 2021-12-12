b9ce5485bd1b187e56b2d8ed08fde316228753b3
Javier Herraiz <elperrochico@gmail.com>
2021-12-12 20:50:57 +0100

    test error

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -10,7 +10,13 @@ class TestCalculate(unittest.TestCase):
         self.calc = Calculator()
 
     def test_add_method_returns_correct_result(self):
-        self.assertEqual(5, self.calc.add(2, 2))
+        self.assertEqual(4, self.calc.add(2, 2))
+        self.assertEqual(0, self.calc.add(2, -2))
+        self.assertEqual(0, self.calc.add(-2, 2))
+        self.assertEqual(1, self.calc.add(1, 0))
+
+    def test_add_method_returns_correct_result(self):
+        self.assertEqual(5, self.calc.add(3, 2))
         self.assertEqual(0, self.calc.add(2, -2))
         self.assertEqual(0, self.calc.add(-2, 2))
         self.assertEqual(1, self.calc.add(1, 0))
